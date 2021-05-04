0200517386346a306706ecb87c67d1a288c874d6
xdloc <bretstiles0@gmail.com>
2021-05-04 19:39:18 +0500

    Facade method highlight

--- src/Laravel/Facades/Telegram.php
+++ src/Laravel/Facades/Telegram.php
@@ -4,9 +4,13 @@ namespace Telegram\Bot\Laravel\Facades;
 
 use Telegram\Bot\BotsManager;
 use Illuminate\Support\Facades\Facade;
+use Telegram\Bot\Objects\Update;
 
 /**
  * Class Telegram.
+ *
+ * @method static Update|Update[] commandsHandler(bool $webhook = false)
+
  */
 class Telegram extends Facade
 {
